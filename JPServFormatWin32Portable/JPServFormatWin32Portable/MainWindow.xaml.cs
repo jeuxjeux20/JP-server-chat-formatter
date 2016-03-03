1dded8fd017218812fe9922ba900a608fa4ddf97
jeuxjeux20 <yaniaitamara2@yahoo.fr>
2016-03-03 20:12:46 +0100

    Updated icon and release

--- JPServFormatWin32Portable/JPServFormatWin32Portable/MainWindow.xaml.cs
+++ JPServFormatWin32Portable/JPServFormatWin32Portable/MainWindow.xaml.cs
@@ -18,8 +18,10 @@ namespace JPServFormatWin32Portable
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
