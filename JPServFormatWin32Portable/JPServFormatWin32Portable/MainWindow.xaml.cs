a493eb5fc45c6fc02dfb24d2936da66b75eefb7b
jeuxjeux20 <yaniaitamara2@yahoo.fr>
2016-03-04 16:04:46 +0100

    Version 2.0.0.0 : Added ability to change the size of the text

--- JPServFormatWin32Portable/JPServFormatWin32Portable/MainWindow.xaml.cs
+++ JPServFormatWin32Portable/JPServFormatWin32Portable/MainWindow.xaml.cs
@@ -19,21 +19,39 @@ namespace JPServFormatWin32Portable
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
     /// 
+
     public partial class MainWindow : Window
     {
 
         public MainWindow()
         {
             InitializeComponent();
+
+            big.IsChecked = true;
         }
-        
+        private Dictionary<string, int> sizes { get; } = new Dictionary<string, int>() { ["small"] = 10, ["medium"] = 20, ["big"] = 30 };
+        public int size
+        {
+            get
+            {
+                foreach (MenuItem item in Size.Items)
+                {
+                    if (item.IsChecked)
+                    {
+                        return sizes[item.Name];
+                    }
+                }
+                return 30;
+            }
+        }
         private void button_Click(object sender, RoutedEventArgs e)
+        
         {
             if (ip.Text.Contains("ws://"))
             {
-                ip.Text = ip.Text.ToString().Substring(ip.Text.IndexOf(@"//") + 2);    
+                ip.Text = ip.Text.ToString().Substring(ip.Text.IndexOf(@"//") + 2);
             }
-            results.Text = @"[size=30][url]http://agar.io/?ip=" + ip.Text + "[/url][/size]";
+            results.Text = $@"[size={size}][url]http://agar.io/?ip=" + ip.Text + "[/url][/size]";
         }
 
         private void copyButton_Click(object sender, RoutedEventArgs e)
@@ -51,11 +69,101 @@ namespace JPServFormatWin32Portable
         private void paste_Click(object sender, RoutedEventArgs e)
         {
             ip.Text = Clipboard.GetText();
+
         }
 
         private void ip_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void big_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void medium_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void small_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+    // ---------------------------------------------------------
+
+
+    public class MenuItemExtensions : DependencyObject
+    {
+        public static Dictionary<MenuItem, string> ElementToGroupNames = new Dictionary<MenuItem, string>();
+
+        public static readonly DependencyProperty GroupNameProperty =
+            DependencyProperty.RegisterAttached("GroupName",
+                                         typeof(string),
+                                         typeof(MenuItemExtensions),
+                                         new PropertyMetadata(string.Empty, OnGroupNameChanged));
+
+        public static void SetGroupName(MenuItem element, string value)
+        {
+            element.SetValue(GroupNameProperty, value);
+        }
+
+        public static string GetGroupName(MenuItem element)
+        {
+            return element.GetValue(GroupNameProperty).ToString();
+        }
+
+        private static void OnGroupNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //Add an entry to the group name collection
+            var menuItem = d as MenuItem;
+
+            if (menuItem != null)
+            {
+                string newGroupName = e.NewValue.ToString();
+                string oldGroupName = e.OldValue.ToString();
+                if (string.IsNullOrEmpty(newGroupName))
+                {
+                    //Removing the toggle button from grouping
+                    RemoveCheckboxFromGrouping(menuItem);
+                }
+                else
+                {
+                    //Switching to a new group
+                    if (newGroupName != oldGroupName)
+                    {
+                        if (!string.IsNullOrEmpty(oldGroupName))
+                        {
+                            //Remove the old group mapping
+                            RemoveCheckboxFromGrouping(menuItem);
+                        }
+                        ElementToGroupNames.Add(menuItem, e.NewValue.ToString());
+                        menuItem.Checked += MenuItemChecked;
+                    }
+                }
+            }
+        }
+
+        private static void RemoveCheckboxFromGrouping(MenuItem checkBox)
+        {
+            ElementToGroupNames.Remove(checkBox);
+            checkBox.Checked -= MenuItemChecked;
+        }
+
+
+        static void MenuItemChecked(object sender, RoutedEventArgs e)
+        {
+            var menuItem = e.OriginalSource as MenuItem;
+            foreach (var item in ElementToGroupNames)
+            {
+                if (item.Key != menuItem && item.Value == GetGroupName(menuItem))
+                {
+                    item.Key.IsChecked = false;
+                }
+            }
         }
     }
 }
